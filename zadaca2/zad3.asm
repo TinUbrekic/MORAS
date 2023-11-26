68492dc406d35295a4a5c8749a3ce15368722ae7
TinUbrekic <149520078+TinUbrekic@users.noreply.github.com>
2023-11-26 13:33:41 +0100

    Update zad3.asm

--- zadaca2/zad3.asm
+++ zadaca2/zad3.asm
@@ -1,16 +1,43 @@
-@0
-D = M;
-@1
-D = D + M;
-@2
-D = D + M;
-@3 
-D = D + M;
-@4
-D = D + M;
-@5 
-M = D;
+@R0
+D = M
+@M1
+D; JLT
+M = D
+@R5 
+M = D + M 
+(M1)
+
+@R1 
+D = M 
+@M2
+D; JLT 
+@R5 
+M = D + M
+(M2)
+
+@R2 
+D = M 
+@M3 
+D; JLT 
+@R5
+M = D + M 
+(M3)
+
+@R3 
+D = M 
+@M4 
+D; JLT 
+@R5 
+M = D + M 
+(M4)
+
+@R4 
+D = M 
+@END 
+D; JLT
+@R5 
+M = D + M
 
 (END)
 @END
-0; JMP
+0;JMP
